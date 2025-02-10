46c8ce7156e8072eab8cec295db289d528b3a3c2
nqt98bh <nqthang.work@gmail.com>
2025-02-10 22:23:51 +0700

    update

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,35 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float secondsPerTurn = 0.5f;
+    private float lastTurnTime = 0;
+    private Quaternion targetRotation;
     private bool isTurning = false;
+
+    private void Start()
+    {
+        targetRotation = transform.rotation;
+    }
     private void Update()
     {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        //transform.Translate(Vector3.forward*speed * Time.deltaTime);
+
+        if (Time.time - lastTurnTime < secondsPerTurn)
+        {
+            float maxDegreeDelta = 90f / secondsPerTurn * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxDegreeDelta);
+            return;
+        }
         //float horizonInput = Input.GetAxis("Horizontal"); 
         float horizonInput = Input.GetAxisRaw("Horizontal");
-        if (!isTurning)
+
+       
+        if (horizonInput > 0 && !isTurning)
         {
-            if (horizonInput > 0 )
-            {
-                Debug.Log("Turning Right");
-                transform.Rotate(0, 90, 0);
-                isTurning = true;
-            }
-            if (horizonInput < 0)
-            {
-                Debug.Log("Turning Left");
-                transform.Rotate(0, -90, 0);
-                isTurning = true;
-
-            }
+            StartTurn(90);
+            isTurning = true;
         }
-
-        transform.position += transform.forward * speed * Time.deltaTime;
+        if (horizonInput < 0 && !isTurning)
+        {
+            StartTurn(-90);
+            isTurning = true;
+        }
         if (horizonInput == 0)
         {
             isTurning = false;
         }
 
+    }
+    private void StartTurn(float angle)
+    {
+        targetRotation *= Quaternion.Euler(0,angle, 0);
+        lastTurnTime = Time.time;
     }
     
 }
